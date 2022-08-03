d8aad72712f2f267fc7548341709e74fa93d7390
박 상목 <ktr2472@gmail.com>
2022-08-03 17:52:53 +0900

    8월3일 opencv 공부

---
+++ chapter2/img_show.py
@@ -0,0 +1,12 @@
+import cv2
+
+img_file = "../img/girl.jpg"  
+img = cv2.imread(img_file)  #파일로부터 이미지 읽기
+
+if img is not None:
+    cv2.imshow('IMG', img)
+    cv2.waitKey()
+    cv2.destroyAllWindwos()
+else:
+    print('No image file')
+    
